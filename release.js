30c46f350bccb7b1453ef7f2167391efc7b4f3eb
LeonidTape <164955846+LeonidTape@users.noreply.github.com>
2025-03-23 15:33:14 +0300

    Update release.js

--- release.js
+++ release.js
@@ -1,16 +1,9 @@
-require('dotenv').config();
-
-const config = {
-  tag_name: "v1.2.3",
-  assets: [
+{
+  "tag_name": "v1.2.3",
+  "assets": [
     {
-      name: "Discord_Bot_Controller.exe",
-      browser_download_url: process.env.DOWNLOAD_URL
+      "name": "Discord_Bot_Controller.exe",
+      "browser_download_url": "https://brigada-blacklist.onrender.com/download/main.exe"
     }
   ]
-};
-
-// Сохраняем JSON в файл или выводим его
-const fs = require('fs');
-fs.writeFileSync('release.json', JSON.stringify(config, null, 2));
-console.log("JSON сгенерирован успешно!");
+}
